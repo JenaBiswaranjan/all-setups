055f52d3c24a10e436bda11475d3004a087831aa
Biswaranjan Jena <160540673+JenaBiswaranjan@users.noreply.github.com>
2024-12-13 12:14:39 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@ sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo amazon-linux-extras install java-openjdk17 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
