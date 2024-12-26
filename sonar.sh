2064ad2bd208249730ece8650cbf7b652f63645f
Biswaranjan Jena <160540673+JenaBiswaranjan@users.noreply.github.com>
2024-12-27 04:23:17 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -10,5 +10,8 @@ chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
 
 #run this on server manually
-#sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
+cd /opt/sonarqube-8.9.6.50800/bin/
+cd linux-x86-64
+./sonar.sh start
+
 #echo "user=admin & password=admin"
